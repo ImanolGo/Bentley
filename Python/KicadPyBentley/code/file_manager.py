7c014cd044939a4df9dd81e5580abcb3bf071093
Imanol Gomez <yo@imanolgomez.net>
2019-03-29 19:23:21 +0100

    started with automatic folder exportation

---
+++ Python/KicadPyBentley/code/file_manager.py
@@ -0,0 +1,203 @@
+import sys
+sys.path.append("libraries/dxfgrabber")
+
+import dxfgrabber
+import os
+import math
+import csv
+
+class FileManager:
+
+    def __init__(self, base_folder, pcbManager):
+        self.pcb = pcbManager
+        self.base_folder = base_folder + "pcbs/"
+        self.__initializeAttributes()
+        print "FileManager::Intitialized"
+
+    def __initializeAttributes(self):
+        pass
+
+    def parse(self):
+        
+        subdirs = [x[0] for x in os.walk(self.base_folder)]
+        #subdirnames = [x[1] for x in os.walk(self.base_folder)]
+        print(subdirs)
+        #print(subdirnames)
+        for i in (1, len(subdirs) - 1):
+            print "FileManager::Reading folder -> " + subdirs[i]
+            self.readDxfFiles(subdirs[i])
+            self.readCsvFiles(subdirs[i])
+            self.savePcb(subdirs[i])
+
+    def savePcb(self, directory):
+        filename = directory.split('/')[-1] 
+        path = directory + '/' + filename + '.kicad_pcb'
+        self.pcb.save(path)
+        self.pcb.clear()
+
+    def readDxfFiles(self, directory):
+
+        files = []   
+        files += [each for each in os.listdir(directory) if each.endswith('.dxf')]
+        for file in files:
+            path = directory + '/' + file
+            self.parseDfx(path)
+
+    def readCsvFiles(self, directory):
+
+        files = []   
+        files += [each for each in os.listdir(directory) if each.endswith(('.csv','.CSV'))]
+        for file in files:
+            path = directory + '/' + file
+            self.parseCsv(path)
+
+
+    def parseCsv(self, path):
+        print "FileManager::parseCsv -> " + path
+        with open(path) as csvfile:
+            readCSV = csv.reader(csvfile, delimiter=',')
+            for row in readCSV:
+                name = row[1].lstrip(' ')
+                coords = [float(row[2]),float(row[3])]
+                angle = float(row[4])
+                side = row[5]
+                self.pcb.addFootprint(name,coords, angle, side)
+
+    def parseDfx(self, path):
+        print "FileManager::parseDfx -> " + path
+        dxf = dxfgrabber.readfile(path)
+
+        for e in dxf.entities:
+
+            typename = e.dxftype
+            #print('FileManager::parseDfx -> Layer: {}\n'.format(e.layer))
+
+            viasStr = "VIAS"
+            edgeStr = "EDGE_CUTS"
+            fcu = "F_CU"
+            bcu = "B_CU"
+            name = "NAME"
+            bcu_plus = "B_CU_PLUS"
+            bcu_minus = "B_CU_MINUS"
+            fcu_plus = "F_CU_PLUS"
+            fcu_minus = "F_CU_MINUS"
+
+            if name in e.layer:
+                pass
+
+            elif viasStr in e.layer:
+                self.parseVia(e)
+
+            elif edgeStr in e.layer:
+                self.parseEdge(e)
+
+            elif bcu_plus in e.layer:
+                self.parseZone(e, 'VI', 'B.Cu')
+
+            elif bcu_minus in e.layer:
+                self.parseZone(e, 'GND', 'B.Cu')
+
+            elif fcu_plus in e.layer:
+                self.parseZone(e, 'VI', 'F.Cu')
+
+            elif fcu_minus in e.layer:
+                self.parseZone(e, 'GND', 'F.Cu')
+
+            elif fcu in e.layer:
+                self.parseTrack(e,'F.Cu')
+
+            elif bcu in e.layer:
+                self.parseTrack(e,'B.Cu')
+
+
+    def parseVia(self, entity):
+
+        typename = entity.dxftype
+        if typename == 'CIRCLE':
+
+            coords = [entity.center[0], entity.center[1]]
+            coords[1] = -coords[1]
+            self.pcb.addVia(coords, entity.radius)
+            #print('FileManager::parseVia -> added via')
+
+    def parseZone(self, entity, net_name, layer):
+
+        #print('FileManager::parseZone -> added polyline -> ' + entity.dxftype)
+        typename = entity.dxftype
+        if typename == 'LWPOLYLINE' or typename == 'POLYLINE':
+            self.pcb.addZone(entity.points,net_name, layer)
+
+    def parseTrack(self, entity, layer):
+
+        typename = entity.dxftype
+    
+        #print('FileManager::parseTrack -> added segment -> layer: ' + layer + ', ' + entity.dxftype)
+
+        if typename == 'LINE':
+
+            start = [entity.start[0], entity.start[1] ]
+            end = [entity.end[0], entity.end[1]]
+            start[1] = -start[1]
+            end[1] = -end[1]
+            self.pcb.addSegment(start, end, layer)
+
+    def parseEdge(self, entity):
+
+        typename = entity.dxftype
+        layer = 'Edge.Cuts'
+
+        #print('FileManager::parseEdge -> added line -> ' + entity.dxftype)
+
+        if typename == 'LWPOLYLINE' or typename == 'POLYLINE':
+
+            for i in range(len(entity.points) - 1 ):
+                start = [entity.points[i][0], entity.points[i][1]]
+                end = [entity.points[i+1][0], entity.points[i+1][1]]
+                start[1] = -start[1]
+                end[1] = -end[1]
+
+                self.pcb.addLine(start, end, layer)
+
+            if entity.is_closed == True:
+                index = len(entity.points) - 1
+                start = [entity.points[index][0], entity.points[index][1]]
+                end = [entity.points[0][0], entity.points[0][1]]
+                start[1] = -start[1]
+                end[1] = -end[1]
+                self.pcb.addLine(start, end, layer)
+
+        elif typename == 'LINE':
+
+            start = [entity.start[0], entity.start[1] ]
+            end = [entity.end[0], entity.end[1]]
+            start[1] = -start[1]
+            end[1] = -end[1]
+            self.pcb.addLine(start, end, layer)
+
+        elif typename == 'SPLINE':
+            for i in range(len(entity.fit_points) - 1 ):
+                start = [entity.fit_points[i][0], entity.fit_points[i][1]]
+                end = [entity.fit_points[i+1][0], entity.fit_points[i+1][1]]
+                start[1] = -start[1]
+                end[1] = -end[1]
+                self.pcb.addLine(start, end, layer) 
+
+        elif typename == 'ARC':
+            # print('FileManager::parseEdge -> start arc angle -> ' + str(entity.start_angle))
+            # print('FileManager::parseEdge -> end arc angle -> ' + str(entity.end_angle))        
+            start = [0.0,0.0]
+            start[0] = entity.center[0] + entity.radius*math.cos(math.radians(entity.start_angle))
+            start[1] = entity.center[1] + entity.radius*math.sin(math.radians(entity.start_angle))
+
+            end = [0.0,0.0]
+            end[0] = entity.center[0] + entity.radius*math.cos(math.radians(entity.end_angle))
+            end[1] = entity.center[1] + entity.radius*math.sin(math.radians(entity.end_angle))
+
+            angle = entity.end_angle - entity.start_angle
+
+            self.pcb.addArc(start, end, angle, layer) 
+
+    
+        
+
+    
---
+++ Python/KicadPyBentley/code/pcb_manager.py
@@ -0,0 +1,115 @@
+import sys
+sys.path.append("libraries/pykicad")
+
+from pykicad.pcb import *
+from pykicad.module import *
+
+class PcbManager:
+
+    def __init__(self, base_folder):
+        self.base_folder = base_folder
+        self.__initializeAttributes()
+        self._setupPcb()
+        print "PcbManager::Intitialized"
+
+    def __initializeAttributes(self):
+        
+        # Define nets
+        self.vi, self.vo, self.gnd = Net('VI'), Net('VO'), Net('GND')
+
+        # Create PCB
+        self.pcb = Pcb()
+        self.clear()
+        self.net_classes = []
+
+    def _setupPcb(self):
+        
+        self.layers = [
+            Layer('F.Cu'),
+            Layer('Inner1.Cu'),
+            Layer('Inner2.Cu'),
+            Layer('B.Cu'),
+            Layer('Edge.Cuts', type='user')
+        ]
+
+        for layer in ['Mask', 'Paste', 'SilkS', 'CrtYd', 'Fab']:
+            for side in ['B', 'F']:
+                self.layers.append(Layer('%s.%s' % (side, layer), type='user'))
+
+        self.net_classes = [NetClass('default', trace_width=1, nets=['VI', 'VO', 'GND'])]
+
+
+    def addVia(self, coords, radius):
+        net = Net()
+        self.nets.append(net)
+        self.vias.append(Via(at=coords, size=radius+0.2, drill=radius, net=net.code))
+
+    def addSegment(self, start, end, layer):
+        net = Net()
+        self.nets.append(net)
+        s = Segment( start=start, end=end, net=net.code, layer = layer)
+        self.segments.append(s)
+
+    def addLine(self, start, end, layer):
+        line = GrLine( start=start, end=end, layer = layer)
+        self.lines.append(line)
+
+    def addArc(self, start, end, angle, layer):
+        arc = GrArc( start=start, end=end, angle = angle, layer = layer)
+        self.arcs.append(arc)
+
+    def addFootprint(self, name, coords, angle, side = 'Front', offset = 0.0):
+        footlog_path = self.base_folder + 'footprints/' + name + '.kicad_mod'
+        m = Module.from_file(footlog_path)
+        if side == 'Back':
+            angle = -angle
+            m.flip()
+            offset = -90
+
+        m.at = coords
+        angle += offset
+        m.rotate(angle)
+        self.modules.append(m)
+
+    def addZone(self, coords, net_name='GND', layer='F.Cu', clearance=0.3):
+        zone = Zone(net_name, layer, coords, clearance)
+        self.zones.append(zone)
+
+    def clear(self):
+        self.vias = []
+        self.segments = []
+        self.polygons = []
+        self.lines = []
+        self.modules = []
+        self.nets = [self.vi, self.vo, self.gnd]
+        self.zones = []
+        self.arcs = []
+        self.polygons = []
+        self.modules = [Module.from_file(self.base_folder + 'footprints/' 'R_0402.kicad_mod')]
+
+
+    def save(self, filename):
+        self.pcb.title = filename
+        self.pcb.comment1 = 'Comment 1'
+        self.pcb.page_type = [20, 20]
+        self.pcb.num_nets = len(self.nets) + 2
+        self.pcb.setup = Setup(grid_origin=[0, 0])
+        self.pcb.layers = self.layers
+        self.pcb.modules = self.modules
+        self.pcb.net_classes = self.net_classes
+        self.pcb.nets = self.nets
+        self.pcb.segments = self.segments
+        self.pcb.vias = self.vias
+        self.pcb.zones = self.zones
+        self.pcb.lines = self.lines
+        self.pcb.arcs = self.arcs
+        self.pcb.polygons = self.polygons
+
+        path =  self.base_folder + filename
+        print("PcbManager:: saving to -> " + path)
+        self.pcb.to_file(filename)
+
+   
+
+
+   
